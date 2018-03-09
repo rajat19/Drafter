cf5ae804d24e31d21b0c747ac3ba0df0ff06486c
rajat <rajatsri94@gmail.com>
2018-03-09 12:58:00 +0530

    Added dj_database_url in requirements

--- Drafter/settings/prod.py
+++ Drafter/settings/prod.py
@@ -1,19 +1,23 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
 ALLOWED_HOSTS.append('draft-machine.herokuapp.com')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
-    }
-}
+if 'USE_LOCAL_DB' in os.environ and os.environ.get('USE_LOCAL_DB') == True:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.postgresql_psycopg2',
+			'NAME': os.environ.get("DB_NAME"),
+			'USER': os.environ.get("DB_USER"),
+			'PASSWORD': os.environ.get("DB_PASSWORD"),
+			'HOST': os.environ.get("DB_HOST"),
+			'PORT': os.environ.get("DB_PORT"),
+		}
+	}
+else:
+	DATABASES['default'] = dj_database_url.config()
 
 MIDDLEWARE_CLASSES.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
