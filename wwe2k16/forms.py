a11d039f7fc29ae5ce08b2a2f0518ec0d4aa1c19
rajat <rajatsri94@gmail.com>
2018-03-23 17:13:57 +0530

    removed fields from form

--- wwe2k16/forms.py
+++ wwe2k16/forms.py
@@ -1,9 +1,7 @@
 from .models import Wrestler, TagTeam, Championship, Event, Match, TagTeamMatch, MatchType
-from django import forms, template
+from django.forms import ModelForm, TextInput, CharField, HiddenInput
 
-class WrestlerForm(forms.ModelForm):
-	fields = ['name', 'ovr', 'country', 'brand', 'height', 'weight', 'original_primary', 'original_secondary', 'primary', 'secondary', 'tertiary', 'tag_team']
-
+class WrestlerForm(ModelForm):
 	def __init__(self, *args, **kwargs):
 		super(WrestlerForm, self).__init__(*args, **kwargs)
 		self.fields['brand'].empty_label = ''
@@ -12,37 +10,39 @@ class WrestlerForm(forms.ModelForm):
 		model = Wrestler
 		fields = ['name', 'ovr', 'country', 'brand', 'height', 'weight', 'original_primary', 'original_secondary', 'primary', 'secondary', 'tertiary', 'tag_team']
 
-class TagTeamForm(forms.ModelForm):
+class TagTeamForm(ModelForm):
 	class Meta:
 		model = TagTeam
 		fields = ['name', 'members']
 
-class ChampionshipForm(forms.ModelForm):
+class ChampionshipForm(ModelForm):
 	class Meta:
 		model = Championship
 		fields = ['name', 'belt_type', 'status', 'champion']
 
-class MatchForm(forms.ModelForm):
-	fields = ['event', 'championship', 'match_type', 'participants', 'winner']
-
+class MatchForm(ModelForm):
 	def __init__(self, *args, **kwargs):
+		exclude_event = kwargs.pop('exclude_event', None)
 		super(MatchForm, self).__init__(*args, **kwargs)
 		self.fields['event'].empty_label = ''
 		self.fields['championship'].empty_label = ''
 		self.fields['match_type'].empty_label = ''
-		self.fields['winner'] = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'autocomplete'}))
+		self.fields['winner'] = CharField(required=False, widget=TextInput(attrs={'class': 'autocomplete'}))
+		if exclude_event:
+			self.fields['event'].widget = HiddenInput()
 
 	class Meta:
 		model = Match
 		fields = ['event', 'championship', 'match_type', 'participants']
-
-class TagMatchForm(forms.ModelForm):
-	fields = ['event', 'championship', 'team1', 'team2', 'winner']
-
+		
+class TagMatchForm(ModelForm):
 	def __init__(self, *args, **kwargs):
+		exclude_event = kwargs.pop('exclude_event', None)
 		super(TagMatchForm, self).__init__(*args, **kwargs)
 		self.fields['event'].empty_label = ''
 		self.fields['championship'].empty_label = ''
+		if exclude_event:
+			self.fields['event'].widget = HiddenInput()
 
 	class Meta:
 		model = TagTeamMatch
